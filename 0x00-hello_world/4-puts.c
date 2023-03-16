60368e3d1a8100fb6b13be527842ae7c5e586ed6
Abel-mwirigi <mwirigiabel223359@daystar.ac.ke>
2023-03-17 01:55:52 +0300

    Compiler

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /**
- * main - Entry point a program thats prints
+ * main - Entry points a program thats prints a line with puts function
  *
- * Return: Always 0 (Success) 
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
