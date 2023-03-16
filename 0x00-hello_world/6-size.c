a4806d8a1d29601127e4fb06838b0598321d4e5d
Abel-mwirigi <mwirigiabel223359@daystar.ac.ke>
2023-03-17 02:53:07 +0300

    Compiler

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,9 +12,9 @@ int main(void)
 	long long int b;
 	float c;
 
-printf("Size of a char:%zu byte(s)\n",(unsigned long)sizeof(y));printf("Size of a int:%zu byte(s)\n",(unsigned long)sizeof(x));
-printf("Size of a long int:%zu byte(s)\n",(unsigned long)sizeof(a));
-printf("Size of a long long int:%zu byte(s)\n",(unsigned long)sizeof(b));
-printf("Size of a float:%zu byte(s)\n",(unsigned long)sizeof(c));
+printf("Size of a char:%lu byte(s)\n",(unsigned long)sizeof(y));printf("Size of a int:%lu byte(s)\n",(unsigned long)sizeof(x));
+printf("Size of a long int:%lu byte(s)\n",(unsigned long)sizeof(a));
+printf("Size of a long long int:%lu byte(s)\n",(unsigned long)sizeof(b));
+printf("Size of a float:%lu byte(s)\n",(unsigned long)sizeof(c));
 return (0);
 }
