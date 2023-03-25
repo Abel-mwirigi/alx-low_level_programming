bff8953058effe1128619066918cdda558d5801b
Abel-mwirigi <mwirigiabel223359@daystar.ac.ke>
2023-03-25 05:21:09 +0300

    Numbers constitute the only universal language

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - product of two numbers
+ * @a: the first variable
+ * @b: second variable
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+
+	int result = a * b;
+
+	return (result);
+}
