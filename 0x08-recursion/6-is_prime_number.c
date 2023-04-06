d4d2c8b7d81dbf3b7ac019369f65816ebfd460d0
Abel-mwirigi <mwirigiabel223359@daystar.ac.ke>
2023-04-06 21:00:17 +0300

    prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ * is_prime_number - checks for prime numbers
+ * @n:number to be checked
+ * Return:Always 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n / 2 != 0)
+		return (1);
+	return (0);
+}
