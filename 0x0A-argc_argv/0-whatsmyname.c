c89411ad8c1d92f0aa27e6260295a94dfee7f5ab
Abel-mwirigi <mwirigiabel223359@daystar.ac.ke>
2023-04-10 11:44:58 +0300

    mynewname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main (int argc, char *argv[])
+/**
+ * main - program
+ * @argc:number of parameters
+ * @argv:array of pointers
+ * Return:Always 0
+ */
+int main(int argc, char *argv[])
 {
-	printf ("%s\n", argv[0]);
-	return 0;
+	printf("%s\n", argv[0]);
+	return (0);
 }
