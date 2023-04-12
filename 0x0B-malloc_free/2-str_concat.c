b477ad94808fcc249132f6beb0e9b3860fb3f9d2
Abel-mwirigi <mwirigiabel223359@daystar.ac.ke>
2023-04-12 20:13:07 +0300

    concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,15 +23,15 @@ char *str_concat(char *s1, char *s2)
 	for (len2 = 0; s2[len2] != '\0'; len2++)
 	{
 	}
-	ar = malloc((len1 + len2 + 2) * sizeof(char));
+	ar = malloc((len1 + len2 + 1) * sizeof(char));
 	if (ar == NULL)
 		return (NULL);
 
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; i < len1; i++)
 	{
 		ar[i] = s1[i];
 	}
-	for (j = 0; s2[j] != '\0'; j++)
+	for (j = 0; j < len2; j++)
 	{
 		ar[i + 1] = s2[j];
 	}
