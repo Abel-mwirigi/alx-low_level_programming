0e6c57aaffc8934eeaece079d936b9889b3aacee
Abel-mwirigi <mwirigiabel223359@daystar.ac.ke>
2023-04-22 22:29:05 +0300

    wueh

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * free_dog - function that frees dogs
+ * @d:pointer
+ * Return:void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d);
+	free(d->owner);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,4 +16,13 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - typedef
+ */
+typedef struct dog dog_t;
+
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
