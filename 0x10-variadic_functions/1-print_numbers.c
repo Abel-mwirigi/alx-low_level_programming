6772babba376e2f397b7e5ec4fb3da7293d5a59a
Abel-mwirigi <mwirigiabel223359@daystar.ac.ke>
2023-04-29 23:44:57 +0300

    commit to add files on variadic functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -4,6 +4,7 @@
  * print_numbers - prints numbers, followed by a new line
  * @separator: string to be printed between numbers
  * @n: number of integers passed to the function
+ * Return: void
  */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
@@ -15,13 +16,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		if (!separator)
-			printf("%d", va_arg(list, int);
-					else if (separator && i == 0)
-					printf("%d", va_arg(list, int));
-					else
-					printf("%s", separator);
+		printf("%d", va_arg(list, int));
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
 	}
-	va_end(list);
+
 	printf("\n");
+	va_end(list);
 }
+
