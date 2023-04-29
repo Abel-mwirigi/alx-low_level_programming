767dd6575228ea802c86e40150da3a7e2f49a92c
Abel-mwirigi <mwirigiabel223359@daystar.ac.ke>
2023-04-29 22:05:23 +0300

    commit to add files on variadic functions

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,49 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
+ *
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list list;
+	int i = 0;
+	char *s;
+
+	va_start(list, format);
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%c", va_arg(list, int));
+					break;
+				case 'i':
+					printf("%d", va_arg(list, int));
+					break;
+				case 'f':
+					printf("%f", va_arg(list, double));
+					break;
+				case 's':
+					s = va_arg(list, char *);
+					if (s == NULL)
+						s = "(nil)";
+					printf("%s", s);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			if (format[i + 1])
+				printf(", ");
+			i++;
+		}
+	}
+		va_end(list);
+		printf("\n");
+}
