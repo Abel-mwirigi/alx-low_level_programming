10a94d968de162684d42a01e4c1445a3d1ed77fb
Abel-mwirigi <mwirigiabel223359@daystar.ac.ke>
2023-06-16 06:05:14 +0300

    commit to add concatenating function

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,7 @@
 #include"main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
  * string_nconcat - function that concatenates two strings
@@ -11,25 +14,30 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ar;
-	unsigned int i, j, k, l;
+	unsigned int i, j, len1, len2;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (i = 0; s1[i] != '\0'; i++)
-		;
-	for (j = 0; s2[j] != '\0'; j++)
-		;
-	if (n >= j)
-		n = j;
-	ar = malloc((n + i + 1) * sizeof(char));
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+	if (n >= len2)
+	{
+		n = len2;
+	}
+	ar = malloc((n + len1 + 1) * sizeof(char));
 	if (ar == NULL)
 		return (NULL);
-	for (k = 0; k < i; k++)
-		ar[k] = s1[k];
-	for (l = 0; l < n; l++)
-		ar[k + l] = s2[l];
-	ar[k + l] = s2[l];
+	for (i = 0; i < len1; i++)
+	{
+		ar[i] = s1[i];
+	}
+	for (j = 0; j < n; j++)
+	{
+		ar[i + j] = s2[j];
+	}
+	ar[i + j] = '\0';
 	return (ar);
 }
